bf08f5c3940615fe03b9f31a0f472aaec6fdc6c4
Yusuf Said Aktan <contact@ysaktan.com>
2025-02-02 21:41:43 +0300

    fix: syntax

--- src/App.java
+++ src/App.java
@@ -77,10 +77,10 @@ public class App {
 
     private static void printIntegerArray (int [] arr) {
         System.out.print("( ");
-        for (i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             System.out.print(arr[i] + ", ");
         }
-        System.out.println(arr[length - 1] + " )");
+        System.out.println(arr[arr.length - 1] + " )");
     }
 
     // Print min and max of array(aka c.)
